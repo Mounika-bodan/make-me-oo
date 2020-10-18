13a181497951b4a6001bff904bc2b69c8344e8d7
Mounika Bodanapu <mounimounika286@gmail.com>
2020-10-18 17:05:09 +0530

    modified to oop

--- src/main/java/org/oop/DistanceAndDirectionCalculator.java
+++ src/main/java/org/oop/DistanceAndDirectionCalculator.java
@@ -1,15 +1,27 @@
 package org.oop;
 
 public class DistanceAndDirectionCalculator {
-    public static double distance(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
-        return Math.sqrt(Math.pow(xDistance, 2) + Math.pow(yDistance, 2));
+    public static double xDifference;
+    public static double yDifference;
+
+    public static  void setCoordinatesDifference(Point start, Point end) {
+        xDifference = end.getXdifference (start);
+        yDifference = end.getYdifference (start);
     }
 
-    public static double direction(Point from, Point to) {
-        double xDistance = to.getX() - from.getX();
-        double yDistance = to.getY() - from.getY();
-        return Math.atan2(yDistance, xDistance);
+    public static double distance(Point start, Point end) {
+        setCoordinatesDifference (start, end);
+        return Math.sqrt (square (xDifference) + square (yDifference));
     }
+
+    public static double direction(Point start, Point end) {
+        setCoordinatesDifference (start, end);
+        return Math.atan2 (yDifference, xDifference);
+    }
+
+    private static double square(double difference) {
+        return Math.pow (difference, 2);
+    }
+
+
 }
--- src/main/java/org/oop/Point.java
+++ src/main/java/org/oop/Point.java
@@ -3,25 +3,28 @@ package org.oop;
 public class Point {
     private double x;
     private double y;
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
-    }
-
     public Point(double x, double y) {
-        this.x = x;
+        this.setX (x);
+        this.setY (y);
+    }
+    public double getX() {
+        return this.x;
+    }
+    public double getY() { return this.y;}
+
+    private  void setY(double y) {
         this.y = y;
+    }
+    private  void setX(double x) {
+        this.x = x;
+    }
+
+    public double getXdifference(Point start)
+    {
+        return this.getX ( ) - start.getX ( );
+    }
+    public double getYdifference(Point start)
+    {
+        return this.getY ( ) - start.getY ( );
     }
 }
